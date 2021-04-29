44e8f98b30d7dc017228629018aa7e50d6fe5ca3
pcmbryo <pcmbr.magic@gmail.com>
2021-04-29 21:29:02 +0900

    Initial Commit

---
+++ SCCC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SCCC
+//
+//  Created by Ryo UEMURA on 2021/04/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
